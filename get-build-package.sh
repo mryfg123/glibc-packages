4c699e0e532ebf8cb9c56feb30d2a94704b24039
mryfg123 <sd12309@outlook.com>
2026-08-09 21:09:46 +0800

    Change git repository source for package cloning

--- get-build-package.sh
+++ get-build-package.sh
@@ -4,8 +4,7 @@
 
 BRANCH="master"
 
-git clone --depth 1 -b ${BRANCH} --single-branch https://github.com/termux/termux-packages.git
-
+git clone --depth 1 -b ${BRANCH} --single-branch https://github.com/mryfg123/termux-packages.git
 for i in build-package.sh clean.sh packages x11-packages root-packages scripts ndk-patches; do
 	rm -fr ./${i}
 	cp -r ./termux-packages/${i} ./
